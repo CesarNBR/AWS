a0936c4311b71d1192321d0b7317ef8d1bcd04af
CesarNBR <buendia732@gmail.com>
2021-06-02 15:00:39 -0500

    Modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res) => res.send('Hello world with express!'))
+app.get('/',(req,res) => res.send('<h1>Proyecto microprocesadores con HTML!</h1>'))
 
 app.listen(3000); 
 console.log('Server on Port 3000')
