7f4d1255b21b0d823727ed9a2e2101d4e9c313f2
CesarNBR <buendia732@gmail.com>
2021-06-02 16:27:39 -0500

    port 8000

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/',(req,res) => res.send('<h1>Proyecto microprocesadores con HTML!</h1>'))
 
-app.listen(8080); 
-console.log('Server on Port 8080')
+app.listen(8000); 
+console.log('Server on Port 8000')
